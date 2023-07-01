443d42f277985a64a57f20353dd11f331cc09a8b
mkb <anmolkb1020@gmail.com>
2023-07-01 18:20:49 +0500

    working on Patient Registration page

--- frontend/src/Pages/PatientRegister.js
+++ frontend/src/Pages/PatientRegister.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import axios from "axios";
 
+
 const PatientRegister = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
@@ -37,10 +38,91 @@ const PatientRegister = () => {
   };
 
   return (
-    <div className="background pt-2">
+    <div className="background pt-2 pb-5 ">
       <h1 className="mt-5 text-center tagline ">Patient Registration</h1>
 
+      <section className="vh-25 " >
+        <div className="container h-50">
+          <div className="row d-flex justify-content-center align-items-center h-100">
+            <div className="col-lg-12 col-xl-11">
+              <div className="card text-black cardBorder transparentCard border mt-5 " >
+                <div className="card-body p-md-1">
+                  <div className="row justify-content-center">
+                    <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1 ">
 
+                      <p className=" h1 fw-bold mb-5 mx-1 mx-md-4 mt-4 formText ps-3">Enter your details</p>
+
+                      <form className="mx-1 mx-md-4 formText">
+
+                        <div className="d-flex flex-row align-items-center mb-4">
+                          <i className="fas fa-user fa-lg me-3 fa-fw"></i>
+                          <div className="form-outline flex-fill mb-0">
+                            <input type="text" id="name" placeholder="Name" className="form-control"/>
+
+                          </div>
+                        </div>
+
+                        <div className="d-flex flex-row align-items-center mb-4">
+                          <i className="fas fa-user fa-lg me-3 fa-fw"></i>
+                          <div className="form-outline flex-fill mb-0">
+                            <input type="number" id="age" placeholder="Age" className="form-control" readOnly/>
+
+                          </div>
+                        </div>
+
+
+                        <div className="d-flex flex-row align-items-center mb-4">
+                          <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                          <div className="form-outline flex-fill mb-0">
+                            <input type="email" id="email" className="form-control" placeholder="Email"/>
+
+                          </div>
+                        </div>
+
+                        <div className="d-flex flex-row align-items-center mb-4">
+                          <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
+                          <div className="form-outline flex-fill mb-0">
+                            <input type="password" id="password" className="form-control" placeholder="Password"/>
+
+                          </div>
+                        </div>
+
+                       <div className="d-flex flex-row align-items-center mb-4">
+                          <i className="fas fa-user fa-lg me-3 fa-fw"></i>
+                          <div className="form-outline flex-fill mb-0">
+                            <input type="text" id="city" className="form-control" placeholder="City"/>
+                          </div>
+                        </div>
+
+                        <div className="d-flex flex-row align-items-center mb-4">
+                          <i className="fas fa-user fa-lg me-3 fa-fw"></i>
+                          <div className="form-outline flex-fill mb-0">
+                            <input type="text" id="phone_no" className="form-control" placeholder="Phone Number"/>
+                          </div>
+                        </div>
+
+
+                        <div className="d-flex mx-4 mb-3 mb-lg-4">
+                          <button type="button" className="btn customButton">Register</button>
+                        </div>
+
+                      </form>
+
+                    </div>
+                    <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2 ">
+
+                      <img src={process.env.PUBLIC_URL + "/images/regPatient.png"}
+                            className="img-fluid pe-5"
+                            alt="patient registration"/>
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
 
 
 
