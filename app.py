5d6c447a09852d09b2931044179fa03a50cd8f4f
Muhammad Usman <usmanshoaib537@gmail.com>
2023-07-22 14:57:44 +0500

    Login routes are added

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request, render_template
 from database import dbinitialization
+from database.models import patients, doctors
 from flask_restful import Api
 from resources import routes
 from flask_session import Session
@@ -31,6 +32,59 @@ def patientRegistration():
 @app.route('/doctorRegistration')
 def doctorRegistration():
     return render_template("doctorRegistration.html")
+
+
+@app.route('/patientlogin')
+def patientlogin():
+    return render_template("patientLogin.html")
+
+
+@app.route('/doctorlogin')
+def doctorlogin():
+    return render_template("doctorLogin.html")
+
+
+@app.route('/patientLogin', methods=['POST'])
+def patientLogin():
+    try:
+        email = request.form['email']
+        password = request.form['password']
+        login_check = patients.objects(email=email, password=password).first()
+        if login_check:
+            approval_check = patients.objects(email=email, password=password, status='approved').first()
+            rejection_check = patients.objects(email=email, password=password, status='rejected').first()
+            if approval_check:
+                return {'message': 'Login Successful'}, 200
+            elif rejection_check:
+                return {'message': 'Sorry to inform that, your registration request has been rejected.'}, 403
+            else:
+                return {'message': 'Your Registration Request is PENDING at the moment.'}, 403
+        else:
+            return {'message': 'No account found with these credentials.'}, 404
+    except Exception as e:
+        return {'message': f'error occurred due to {str(e)}'}, 404
+
+
+@app.route('/doctorLogin', methods=['POST'])
+def doctorLogin():
+    try:
+        email = request.form['email']
+        password = request.form['password']
+        login_check = doctors.objects(email=email, password=password).first()
+        if login_check:
+            approval_check = doctors.objects(email=email, password=password, status='approved').first()
+            rejection_check = doctors.objects(email=email, password=password, status='rejected').first()
+            if approval_check:
+                return {'message': 'Login Successful'}, 200
+            elif rejection_check:
+                return {'message': 'Sorry to inform that, your registration request has been rejected.'}, 403
+            else:
+                return {'message': 'Your Registration Request is PENDING at the moment.'}, 403
+        else:
+            return {'message': 'No account found with these credentials.'}, 404
+    except Exception as e:
+        return {'message': f'error occurred due to {str(e)}'}, 404
+
 
 
 # base page
