5f2987a07efc22fd6f24fc1f19053ed0335de978
Muhammad Usman <usmanshoaib537@gmail.com>
2023-07-22 19:59:09 +0500

    APIs routes updated

--- resources/routes.py
+++ resources/routes.py
@@ -1,20 +1,10 @@
 from .resources import *
 
 def initialize_routes(api):
-    api.add_resource(RegisterPatient, '/api/patients/register')
-    api.add_resource(RegisterDoctor, '/api/doctors/register')
-    api.add_resource(DoctorLogin, '/api/doctors/login')
-    api.add_resource(PatientLogin, '/api/patients/login')
-    api.add_resource(DoctorSearchByPatient, '/api/patients/search')
-    api.add_resource(ApprovePatientRegistration, '/api/patients/approveRegistration/<patient_id>')
-    api.add_resource(RejectPatientRegistration, '/api/patients/rejectRegistration/<patient_id>')
-    api.add_resource(ApproveDoctorRegistration, '/api/doctors/approveRegistration/<doctor_id>')
-    api.add_resource(RejectDoctorRegistration, '/api/doctors/rejectRegistration/<doctor_id>')
-    api.add_resource(DeletePatient, '/api/patients/delete/<patient_id>')
-    api.add_resource(DeleteDoctor, '/api/doctors/delete/<doctor_id>')
-    api.add_resource(AppointmentRequest, '/api/patients/appointment_request/<doctor_id>')
-    api.add_resource(WithdrawalOfRequest, '/api/patients/appointment_request_withdrawal/<appointment_id>')
-    api.add_resource(PatientAppointmentStatus, '/api/patients/appointment_status/<patient_id>')
-    api.add_resource(DoctorRatings, '/api/patients/doctor_ratings/<doctor_id>')
+    api.add_resource(patientAPI, '/patientAPI/<data>', '/patientAPI/<id>/<status>')
+    api.add_resource(doctorAPI, '/doctorAPI/<data>', "/doctorAPI/<id>/<status>")
+    api.add_resource(searchDoctorsAPI, '/doctors/search')
+    api.add_resource(appointmentAPI, '/appointmentAPI/<data>')
+
 
 
