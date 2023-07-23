768fd5ecab264e7ed4c74584576f0269f6ef8ecd
Muhammad Usman <usmanshoaib537@gmail.com>
2023-07-23 18:53:23 +0500

    URlS are added

--- resources/routes.py
+++ resources/routes.py
@@ -1,8 +1,8 @@
 from .resources import *
 
 def initialize_routes(api):
-    api.add_resource(patientAPI, '/patientAPI/<data>', '/patientAPI/<id>/<status>')
-    api.add_resource(doctorAPI, '/doctorAPI/<data>', "/doctorAPI/<id>/<status>")
+    api.add_resource(patientAPI, '/patientAPI', '/patientAPI/<data>', '/patientAPI/<id>/<status>')
+    api.add_resource(doctorAPI, '/doctorAPI',' /doctorAPI/<data>', "/doctorAPI/<id>/<status>")
     api.add_resource(searchDoctorsAPI, '/doctors/search')
     api.add_resource(appointmentAPI, '/appointmentAPI/<data>')
 
